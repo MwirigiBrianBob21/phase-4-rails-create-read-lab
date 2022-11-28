24e41061ebc12fce185787d5988ca358d27f1c14
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-11-28 13:19:14 +0300

    Soleved error and complete assignment

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,28 @@
+class PlantsController < ApplicationController
+  def index
+    plants = Plant.all
+    render json: plants
+  end
+
+  wrap_parameters format: []
+  def index
+    plants = Plant.all
+    render json: plants
+  end
+
+  def show
+    plant = Plant.find_by(id: params[:id])
+    render json: plant
+  end
+
+  def create
+      plant = Plant.create(params_plant)
+      render json: plant, status: :created 
+  end
+
+  private
+
+  def params_plant
+      params.permit(:name, :image, :price)
+  end  
+end
